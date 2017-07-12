6f040bda24760288a6a28b0b461f1820aa07d2a3
Angel Pino Dominguez <Angel_Pino@ultimatesoftware.com>
2017-07-12 17:29:30 -0400

    Basic Builds with Webpack

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,2 @@
+//This is global JS provided to all apps.
 console.log('logging from utils.js file...');
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,24 @@ module.exports = {
     watch: true,
     devServer: {
         disableHostCheck: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "jshint-loader"
+            }
+        ],
+        loaders: [
+            {
+                test: /\.es6$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.es6']
     }
 }
